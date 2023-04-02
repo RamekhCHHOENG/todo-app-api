b606d097bf3841a31adb0b2369ce0eded3310d72
RamekhCHHOENG <ramekhchhoeng@icloud.com>
2023-04-02 21:18:14 +0700

    chore: update default port dotenv file

--- src/app.js
+++ src/app.js
@@ -23,4 +23,5 @@ app.use(require("./routes"));
 app.use(require("./routes/todo"));
 
 // server configurations....
-app.listen(3000, () => console.log("Server started listening on port: 3000"));
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log("Server started listening on port: 3000"));
