bc9cb68cb82b707a53d3e7750576ce4af75a0777
RamekhCHHOENG <ramekhchhoeng@icloud.com>
2023-04-02 17:21:19 +0700

    feat: update project structure

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // routes
-app.use(require("./routes/index"));
+app.use(require("./routes"));
 app.use(require("./routes/todo"));
 
 // server configurations....
